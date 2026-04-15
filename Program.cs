45d901493ce8db52d70ebdea3f793b6c5518ff31
Aryan Hule <aryanhule@gmail.com>
2026-04-15 08:29:22 +0530

    Cors Issue fix

--- Program.cs
+++ Program.cs
@@ -31,6 +31,7 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins(
                 "http://localhost:5173",
+                "http://localhost:4173",
                 "https://your-app.vercel.app"
             )
             .AllowAnyHeader()
